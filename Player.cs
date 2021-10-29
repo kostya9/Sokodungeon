01b789016b98184935a5cc7254241d664e77128c
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2021-10-29 23:52:11 +0300

    Add movement queue

--- Player.cs
+++ Player.cs
@@ -1,11 +1,54 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dungeon;
 
 public class Player : Spatial
-{	
+{
+	private readonly Queue<InputEvent> _unprocessedInputEvents = new();
+	private readonly Dictionary<uint, List<string>> _actionsByKeycode = new();
+	private Tween _tween;
+
 	public override void _Ready()
 	{
-		
+		_tween = (Tween) GetNode("Tween");
+
+		InitializeKeyToActionMapping();
+	}
+
+	private void InitializeKeyToActionMapping()
+	{
+		foreach (string action in InputMap.GetActions())
+		{
+			foreach (InputEvent inputEvent in InputMap.GetActionList(action))
+			{
+				if (inputEvent is InputEventKey key)
+				{
+					if (!_actionsByKeycode.TryGetValue(key.Scancode, out var actionsList))
+					{
+						actionsList = new List<string>();
+
+						_actionsByKeycode[key.Scancode] = actionsList;
+					}
+
+					actionsList.Add(action);
+				}
+			}
+		}
+	}
+
+	public override void _Process(float delta)
+	{
+		if (!_tween.IsActive())
+		{
+			if (_unprocessedInputEvents.Count > 0)
+			{
+				var unprocessedEvent = _unprocessedInputEvents.Dequeue();
+				
+				ProcessInput(unprocessedEvent);
+			}
+		}
 	}
 
 	public Spatial GetCameraHolder()
@@ -25,19 +68,45 @@ public class Player : Spatial
 	
 	public override void _Input(InputEvent @event)
 	{
+		ProcessInput(@event);
+		
+		base._Input(@event);
+	}
+
+	private void ProcessInput(InputEvent @event)
+	{
 		if (@event is InputEventKey key)
 		{
-			Move(key);
-			Rotate(key);
+			if (!_actionsByKeycode.TryGetValue(key.Scancode, out var actions))
+			{
+				return;
+			}
+
+			var isTweenActive = _tween.IsActive();
+
+			foreach (var action in actions)
+			{
+				if (isTweenActive)
+				{
+					const int queueLimit = 2;
+					if (Input.IsActionJustPressed(action) && _unprocessedInputEvents.Count < queueLimit)
+					{
+						_unprocessedInputEvents.Enqueue(key);
+					}
+				}
+				else
+				{
+					Move(key, action);
+					Rotate(key, action);	
+				}
+			}
 		}
 
 		if (@event is InputEventMouseButton mouseButton)
 		{
 			Console.WriteLine("click");
 			Raycast(mouseButton);
-		}
-		
-		base._Input(@event);
+		}	
 	}
 
 	private void Raycast(InputEventMouseButton mouseButton)
@@ -74,23 +143,16 @@ public class Player : Spatial
 	}
 	
 
-	private void Move(InputEventKey key)
+	private void Move(InputEventKey key, string action)
 	{
-		var tween = (Tween) GetNode("Tween");
-
-		if (tween.IsActive())
-		{
-			return;
-		}
-		
 		var forward = Transform.basis.x.Normalized();
 
-		var movementVector = (KeyList) key.Scancode switch
+		var movementVector = action switch
 		{
-			KeyList.Up => forward,
-			KeyList.Down => -forward,
-			KeyList.Left => -forward.Cross(Vector3.Up),
-			KeyList.Right => forward.Cross(Vector3.Up),
+			DungeonActions.Forwards => forward,
+			DungeonActions.Backwards => -forward,
+			DungeonActions.Left => -forward.Cross(Vector3.Up),
+			DungeonActions.Right => forward.Cross(Vector3.Up),
 			_ => Vector3.Zero
 		};
 		
@@ -103,7 +165,7 @@ public class Player : Spatial
 		if (!CanMoveInto(endpoint))
 			return;
 
-		tween.InterpolateProperty(this, "translation",
+		_tween.InterpolateProperty(this, "translation",
 			Translation,
 			endpoint,
 			0.2f,
@@ -111,25 +173,18 @@ public class Player : Spatial
 			Tween.EaseType.InOut);
 
 		GetCameraAnimationPlayer().Play("Step", -1f, 5);
-		tween.Start();
+		_tween.Start();
 	}
 	
-	private void Rotate(InputEventKey key)
+	private void Rotate(InputEventKey key, string action)
 	{
-		var tween = (Tween) GetNode("Tween");
-
-		if (tween.IsActive())
-		{
-			return;
-		}
-		
 		var leftRotation = new Vector3(0, 90, 0);
 		var rightRotation = -leftRotation;
 		
-		var rotVector = (KeyList) key.Scancode switch
+		var rotVector = action switch
 		{
-			KeyList.Q => leftRotation,
-			KeyList.E => rightRotation,
+			DungeonActions.RotateLeft => leftRotation,
+			DungeonActions.RotateRight => rightRotation,
 			_ => Vector3.Zero
 		};
 
@@ -137,14 +192,14 @@ public class Player : Spatial
 			return;
 		
 
-		tween.InterpolateProperty(this, "rotation_degrees",
+		_tween.InterpolateProperty(this, "rotation_degrees",
 			RotationDegrees,
 			RotationDegrees + rotVector,
 			0.2f,
 			Tween.TransitionType.Sine,
 			Tween.EaseType.InOut);
 
-		tween.Start();
+		_tween.Start();
 	}
 
 	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
