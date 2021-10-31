bceda7ec5f0bf0b78b8e2704c463cd89783a2041
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2021-10-31 16:09:38 +0200

    Add MapGen

--- MapGen.cs
+++ MapGen.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MapGen : Node
 {
@@ -7,51 +9,246 @@ public class MapGen : Node
 	// private int a = 2;
 	// private string b = "text";
 
+	private string map = @"
+   - - - 
+|p + + +|
+ - - -   
+|o o o|+|
+         
+|o o o|+|
+         
+|o o o|+|
+         
+|o o o|+|
+         
+|o o o|+|
+ - - -   
+ + + + +|
+ - - - -
+";
+
+	public struct Map
+	{
+		public Cell[] Cells;
+		
+		public Wall[] Walls;
+		public int PlayerX { get; set; }
+		public int PlayerY { get; set; }
+	}
+
+	public struct Wall
+	{
+		public Vector2 FirstPosition;
+
+		public Vector2 SecondPosition;
+
+		public WallType Type;
+	}
+
+	public enum WallType
+	{
+		Vertical, Horizontal
+	}
+
+	public struct Cell
+	{
+		public Vector2 Position;
+	}
+
+	public Map ParseMap(string map)
+	{
+		map = map.Replace("\r\n", "\n");
+		
+		var mapSpan = map.AsSpan().Trim('\n');
+
+		var width = mapSpan.IndexOf('\n') + 1;
+		var height = 1 + (int)Math.Ceiling((mapSpan.LastIndexOf('\n')) / (double)width);
+
+		List<Cell> cells = new List<Cell>();
+		List<Wall> walls = new List<Wall>();
+
+		int x = 0;
+		int y = 0;
+
+		int playerX = 0, 
+			playerY = 0;
+
+		for (int row = 0; row < height; row++)
+		{
+			for (int col = 0; col < width; col++)
+			{
+				var idx = col + row * width;
+				if (idx >= mapSpan.Length)
+					break;
+				
+				var current = mapSpan[col + row * width];
+
+				if (row % 2 == 0) // Horizontal walls
+				{
+					if (current == '-')
+					{
+						walls.Add(new Wall
+						{
+							FirstPosition = new Vector2(x, y - 1),
+							SecondPosition = new Vector2(x, y),
+
+							Type = WallType.Horizontal
+						});
+
+						x++;
+					}
+					else if (current == ' ')
+					{
+						if (col % 2 == 1)
+						{
+							x++;
+						}
+					}
+				}
+				else // Vertical walls, player and cells
+				{
+					if (current == '+')
+					{
+						cells.Add(new Cell
+						{
+							Position = new Vector2(x, y)
+						});
+
+						x++;
+					}
+					else if (current == '|')
+					{
+						walls.Add(new Wall
+						{
+							FirstPosition = new Vector2(x - 1, y),
+							SecondPosition = new Vector2(x, y),
+							
+							Type = WallType.Vertical
+						});
+					}
+					else if (current == 'p')
+					{
+						playerX = x;
+						playerY = y;
+					
+						cells.Add(new Cell
+						{
+							Position = new Vector2(x, y)
+						});
+
+						x++;
+					}
+					else if (current == 'o')
+					{
+						x++;
+					}
+				}
+			}
+
+			if (row % 2 == 1)
+			{
+				y++;	
+			}
+			
+			x = 0;
+		}
+
+		return new Map
+		{
+			Cells = cells.ToArray(),
+			Walls = walls.ToArray(),
+			PlayerX = playerX,
+			PlayerY = playerY
+		};
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var floor = (Spatial) GetParent().GetNode("floor");
-
+		var parsed = ParseMap(map);
+		
 		var player = (Spatial) GetParent().GetNode("Player");
 
-		floor.Translation = player.Translation;
+		var wallResource = (PackedScene)ResourceLoader.Load("res://Scenes/Environment/DungeonWallA.tscn");
+		var floorResource = (PackedScene)ResourceLoader.Load("res://Scenes/Environment/DungeonTileA.tscn");
 
-		var floorMeshInstance = floor.GetChild<MeshInstance>(0);
-		var aabb = floorMeshInstance.Mesh.GetAabb();
 
-		var initX = player.Translation.x;
-		var initZ = player.Translation.z;
+		float xLength = 4f;
+		float yLength = 4f;
 
-		var scaledSize = aabb.Size * floorMeshInstance.Scale;
-		
-		var width = scaledSize.x;
-		var height = scaledSize.y;
+		DrawCells(parsed, floorResource, xLength, yLength);
+		DrawWalls(parsed, xLength, yLength, wallResource);
+	}
 
-		var columns = 100;
-		var rows = 100;
-
-		var centerCol = columns / 2;
-		var centerRow = columns / 2;
-		
-		for (int col = 0; col < columns; col++)
+	private void DrawCells(Map parsed, PackedScene floorResource, float xLength, float yLength)
+	{
+		foreach (var cell in parsed.Cells)
 		{
-			for (int row = 0; row < rows; row++)
-			{
-				var posX = initX + col * width - centerCol * width;
-				var posZ = initZ + row * height - centerRow * height;
+			var copy = (Spatial) floorResource.Instance();
 
-				var floorCopy = (Spatial) floor.Duplicate();
-				floorCopy.Translation = new Vector3(posX, player.Translation.y, posZ);
-				floorCopy.Name = $"floor_{col}_{row}";
-				
-				GetParent().CallDeferred("add_child", floorCopy);
-			}
+			var worldX = xLength * cell.Position.x;
+			var worldZ = yLength * cell.Position.y;
+			copy.Translation = new Vector3(worldX, 0f, worldZ);
+
+			AddChild(copy);
 		}
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	private void DrawWalls(Map parsed, float xLength, float yLength, PackedScene wallResource)
+	{
+		var cellsByPosition = parsed.Cells.ToDictionary(x => x.Position);
+		foreach (var wall in parsed.Walls)
+		{
+			var worldFirstX = xLength * wall.FirstPosition.x;
+			var worldFirstZ = yLength * wall.FirstPosition.y;
+
+			var worldSecondX = xLength * wall.SecondPosition.x;
+			var worldSecondZ = yLength * wall.SecondPosition.y;
+
+			var worldX = (worldFirstX + worldSecondX) / 2;
+			var worldZ = (worldFirstZ + worldSecondZ) / 2;
+
+			var translation = new Vector3(worldX, 0, worldZ);
+
+			if (wall.Type == WallType.Vertical)
+			{
+				var top = wall.FirstPosition;
+				if (cellsByPosition.ContainsKey(top))
+				{
+					var copy = (Spatial) wallResource.Instance();
+					copy.Translation = translation;
+					copy.Rotation = new Vector3(0, 3 * Mathf.Pi / 2, 0);
+					AddChild(copy);
+				}
+
+				var bot = wall.SecondPosition;
+				if (cellsByPosition.ContainsKey(bot))
+				{
+					var copy = (Spatial) wallResource.Instance();
+					copy.Translation = translation;
+					copy.Rotation = new Vector3(0, Mathf.Pi / 2, 0);
+					AddChild(copy);
+				}
+			}
+			else
+			{
+				var left = wall.FirstPosition;
+				if (cellsByPosition.ContainsKey(left))
+				{
+					var copy = (Spatial) wallResource.Instance();
+					copy.Translation = translation;
+					copy.Rotation = new Vector3(0, Mathf.Pi, 0);
+					AddChild(copy);
+				}
+
+				var right = wall.SecondPosition;
+				if (cellsByPosition.ContainsKey(right))
+				{
+					var copy = (Spatial) wallResource.Instance();
+					copy.Translation = translation;
+					AddChild(copy);
+				}
+			}
+		}
+	}
 }
